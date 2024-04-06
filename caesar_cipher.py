d14d25bf397cdef0e73f77a7935831fd52fe1e18
Aja Mack <aja.mack@bison.howard.edu>
2024-04-06 02:30:45 +0000

    Add comments

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,10 +1,10 @@
 
 import sys
-
+# Welcome message to encoder
 print("Welcome to the Caesar Cipher Encoder")
 def caesar_cipher(text, shift):
     encoded_text = ""
-
+# Change to Uppercase, shift
     for char in text.upper():
         if char.isalpha():
             shifted = ord(char) - shift % 26
@@ -19,7 +19,7 @@ def caesar_cipher(text, shift):
 def format_output(encoded_text):
     formatted_text = ""
     block_count = 0
-
+# Format per 5 characters, 10 blocks
     for i in range(0, len(encoded_text), 5):
         formatted_text += encoded_text[i:i+5] + " "
         block_count += 1
@@ -32,7 +32,7 @@ def main():
     if len(sys.argv) != 2:
         print("Usage: python3 caesar_cipher.py <shift>")
         sys.exit(1)
-
+# Get input, encode and provide output 
     shift = int(sys.argv[1])
     message = input("Enter a message to encode: ")
     encoded_text = caesar_cipher(message, shift)
