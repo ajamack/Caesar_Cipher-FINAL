54bb58bc840dd576aaa30d5eacf202343e0e015a
Aja Mack <aja.mack@bison.howard.edu>
2024-04-06 01:33:19 +0000

    Add Caesar cipher

---
+++ caesar_cipher.py
@@ -0,0 +1,45 @@
+
+import sys
+
+def caesar_cipher(text, shift):
+    encoded_text = ""
+
+    for char in text.upper():
+        if char.isalpha():
+            shifted = ord(char) + shift % 26
+            if shifted > ord('Z'):
+                shifted -= 26
+            encoded_text += chr(shifted)
+        else:
+            continue
+
+    return encoded_text
+
+def format_output(encoded_text):
+    formatted_text = ""
+    block_count = 0
+
+    for i in range(0, len(encoded_text), 5):
+        formatted_text += encoded_text[i:i+5] + " "
+        block_count += 1
+        if block_count % 10 == 0:
+            formatted_text += "\n"
+
+    return formatted_text.strip()
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 caesar_cipher.py <shift>")
+        sys.exit(1)
+
+    shift = int(sys.argv[1])
+    message = input("Enter a message to encode: ")
+    encoded_text = caesar_cipher(message, shift)
+    formatted_text = format_output(encoded_text)
+
+    print("\nEncoded message:")
+    print(formatted_text)
+
+if __name__ == "__main__":
+    main()
+
